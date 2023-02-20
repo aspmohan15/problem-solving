46697ab6026cc29ee947baf8a0bced98289ed481
mohan.p <mohan.p@codingmart.com>
2023-02-20 11:00:48 +0530

    added

--- temp.js
+++ temp.js
@@ -1,89 +1,11 @@
-{
-    "agency_state_id": 973,
-    "first_name": "C",
-    "last_name": "c",
-    "full_name": "C c",
-    "dial_code": "91",
-    "agency_mobile": "6380142113",
-    "grade": null,
-    "agency_logo": null,
-    "agency_logo_file_name": null,
-    "agency_logo_file_size": null,
-    "agency_name": "Crazy",
-    "agency_email": "crazyagent123@yopmail.com",
-    "am_name": "2HUB Travel",
-    "am_email": "tech@2hub.travel",
-    "am_mobile": "1234567890",
-    "sales_poc_id": 2,
-    "landline_code": null,
-    "landline_number": "",
-    "agency_address": "Erode",
-    "agency_pin_code": "6",
-    "agency_city": "Coimbatore",
-    "agency_state": "Tamil Nadu",
-    "agency_country": "India",
-    "agency_years_in_business": null,
-    "gst_regd": true,
-    "agency_iata_regd": null,
-    "accreditation_id": null,
-    "accreditation": null,
-    "agency_guarantee_mode_id": null,
-    "zone_id": 62,
-    "country_code": "IN",
-    "profile_id": 10,
-    "country_id": 331,
-    "payment_gateway": 1,
-    "pg_surcharge": "1.00",
-    "pan_details": {},
-    "iata_details": {},
-    "gst_details": {
-        "gst_detail_id": 59,
-        "gstin": "07AAGFF2194N1Z1",
-        "gst_certificate": "10_GST_1.png",
-        "gst_status": "Pending",
-        "gst_doc_name": null,
-        "file_name": "dummy.png",
-        "file_size": "",
-        "rejection_reason": null,
-        "place_of_supply_id": null,
-        "destination_name": null,
-        "gst_regd": true,
-        "gstCopy": [
-            {
-                "preview": "https://agents-app.s3.ap-south-1.amazonaws.com/10_GST_1.png",
-                "name": "dummy.png",
-                "size": "",
-                "existing": true,
-                "data": "10_GST_1.png"
-            }
-        ],
-        "gst_file_size": "",
-        "gst_file_name": "dummy.png"
-    },
-    "supporting_doc_details": {},
-    "other_doc_details": {},
-    "company_doc": {
-        "id": 5,
-        "agency_id": 10,
-        "certificate_number": "ccccc",
-        "certificate": "Screenshot from 2023-01-18 10-31-25.png",
-        "submitted_on": "2023-01-10T01:32:45.609Z",
-        "updated_on": "2023-01-21T06:52:10.000Z",
-        "bucket_file_name": "10_COMP_INCORP.png",
-        "is_registered": true,
-        "company_regd_doc": [
-            {
-                "preview": "https://agents-app.s3.ap-south-1.amazonaws.com/10_COMP_INCORP.png",
-                "name": "Screenshot from 2023-01-18 10-31-25.png",
-                "size": "",
-                "existing": true,
-                "data": "10_COMP_INCORP.png"
-            }
-        ],
-        "company_file_size": "",
-        "company_file_name": "Screenshot from 2023-01-18 10-31-25.png"
-    },
-    "other_doc": [],
-    "state_name": "Tamil Nadu",
-    "phone_number": "6380142113"
+function* generatorFunction(i) {
+  yield i;
+
+  yield i + 10;
 }
+
+const generator = generatorFunction(10);
+
+console.log(generator.next());
+console.log(generator.next());
+console.log(generator.next());
